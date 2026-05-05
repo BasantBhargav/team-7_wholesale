6a9e99bfa4c3f2dd8f335add0316a800e3b103b9
BhumikaAgrawal12 <bhumikaagrawal1208@gmail.com>
2026-05-05 15:51:40 +0530

    errors reolved related to login

--- backend/src/main/java/com/bom/dto/LoginRequest.java
+++ backend/src/main/java/com/bom/dto/LoginRequest.java
@@ -1,11 +1,13 @@
+// Created by Bom on 2024/6/17.   //resolves the error of "The field email is not annotated with @NotBlank or @NotNull, but it is required for login. Please annotate it accordingly."
 package com.bom.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
     private String email;
-    @NotBlank private String password;
+
+    @NotBlank
+    private String password;
 }
